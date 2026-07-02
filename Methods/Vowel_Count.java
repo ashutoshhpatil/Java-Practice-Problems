f4b69f4a3a70ba54a3a701a996e83181538da247
Ashutosh Patil <ashutoshpatil1919@gmail.com>
2026-07-02 14:26:18 +0530

    Refactor Vowel_Count class structure

--- Methods/Vowel_Count.java
+++ Methods/Vowel_Count.java
@@ -1,13 +1,9 @@
-package Methods;
-
 public class Vowel_Count {
     String str = "Hello From TekupSkills Training";
     int count =0;
-
     void validcount(){
         for(int i=0;i<str.length();i++) {
             char ch = Character.toLowerCase(str.charAt(i));
-
             if(ch =='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 count++;
             }
