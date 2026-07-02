19ccaed31f072f0a95329eccd86803d8608f2fb7
Ashutosh Patil <ashutoshpatil1919@gmail.com>
2026-07-02 14:25:45 +0530

    Fix formatting issues in Sum_Array.java

--- Methods/Sum_Array.java
+++ Methods/Sum_Array.java
@@ -6,7 +6,7 @@ public class Sum_Array {
     int sum = 0;
     void Add(){
         for(int i =0;i<n;i++){
-            sum = sum+arr[i];
+            sum += arr[i];
         }
         System.out.println("The Sum of the array is:" +sum);
     }
